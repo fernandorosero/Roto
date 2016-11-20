4169924783fc736a60601d528628c026ac6ae440
fernandorosero <tatofarp@hotmail.com>
2016-11-20 11:43:08 +0100

    Subida lenovo PHP

--- js/roto.js
+++ js/roto.js
@@ -1,11 +1,9 @@
 function Adelante(){
     //$('#click-delante').click(function(){
-
-
         // $("#medir").hide('slow'); // oculta el boton
         // $("#load-midiendo").show('slow'); // muestra el load (midiendo)
          //$('#salida').hide('slow'); // oculta la salida (si hay algo)
-        alert("entra");
+   
                             // petición ajax al servidor
         $.ajax({
                 url:'php/delante.php',
@@ -13,7 +11,7 @@ function Adelante(){
                 // Si todo sale bien
                 success: function(response){
                         $('#salida').show('slow'); // muestra la salida (Distancia)
-                       // $('#salida h1').text(response); // muestra la salida (Distancia)
+                        $('#salida h1').text(response); // muestra la salida (Distancia)
                        // $("#medir").show('slow'); // Muestra el boton medir
                        // $("#load-midiendo").hide('slow'); // oculta el load
                        // $('#set-8 h1').text('Volver Medir'); // cambia de texto a "Volver a Medir"
