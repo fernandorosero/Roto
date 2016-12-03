78ab45df0dd3356dd779392fecd7e0961bf19431
fernandorosero <tatofarp@hotmail.com>
2016-12-03 22:05:28 +0100

    Subida lenovo PHP

--- php/web_updown.php
+++ php/web_updown.php
@@ -1,8 +1,5 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    $posicion = $_POST["valorUPDOWN"];
+     echo exec('sudo python /var/www/Roto/python/web_updown.py '.$posicion);
+?>     
 
---
+++ python/web_updown.py
@@ -0,0 +1,20 @@
+from sys import argv
+
+script, posicion = argv
+
+import RPi.GPIO as GPIO    #Importamos la libreria RPi.GPIO
+import time                #Importamos time para poder usar time.sleep
+
+GPIO.setmode(GPIO.BCM)   #Ponemos la Raspberry en modo BOARD
+GPIO.setup(20,GPIO.OUT)    #Ponemos el pin 21 como salida
+p = GPIO.PWM(20,50)        #Ponemos el pin 21 en modo PWM y enviamos 50 pulsos $
+p.start(4.0)               #Enviamos un pulso del 7.5% para centrar el servo
+
+p.ChangeDutyCycle(posicion)    #Enviamos un pulso del 2.8% para subir el ser$
+time.sleep(0.5)           #pausa de medio segundo
+        
+
+print "empieza";
+p.stop()                      #Detenemos el servo
+GPIO.cleanup()                #Limpiamos los pines GPIO de la Raspberry y cerramos el s$
+
