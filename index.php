7e2acfc7164166919de9ec8694fe0bb1b0a30345
fernandorosero <tatofarp@hotmail.com>
2016-12-10 10:46:02 +0100

    Toshiba

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
         <title>Robo MN</title>
         <!-- Estilos -->
         <link rel="stylesheet" type="text/css" href="css/estilos.css"/>
